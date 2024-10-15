282dce6655355dd5b6e3e83d389f2079a7197e2c
saltacloudev01 <saltacloud.dev01@gmail.com>
2024-10-15 16:24:57 -0300

    Mejoras del formulario y mensajes de exito/error

--- send_email.php
+++ send_email.php
@@ -15,37 +15,23 @@
 
     // Cabeceras del correo
     $headers = "From: $name <$email>" . "\r\n" .
-                "Reply-To: $email" . "\r\n" .
-                "X-Mailer: PHP/" . phpversion();
+            "Reply-To: $email" . "\r\n" .
+            "MIME-Version: 1.0" . "\r\n" .
+            "Content-Type: text/plain; charset=UTF-8" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
 
     // Definir el destinatario y el asunto
     $to = "guilletorres81@gmail.com";
-    $subject = "Nuevo mensaje de contacto";
+    $subject = "Contacto con cliente desde formulario";
 
     // Enviar el correo
-    mail($to, $subject, $body,  $headers);
-    header("Location: index.html");
+    if(@mail($to, $subject, $body,  $headers)){
+        @mail($to, $subject, $body,  $headers);
+        header("Location: success-mail.html");
+    }else{
+        header("Location: error-mail.html");
+    }
+
+    
     
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Email enviado</title>
-    <link rel="stylesheet" href="style.css"> <!-- Optional: Link to your CSS -->
-</head>
-<body>
-
-    <h1>Notificaión</h1>
-    <p>Email enviado correctamente</p>
-
-    <!-- Add more content as needed -->
-    <script>
-        setTimeout(() => {
-            window.location.href = "index.html";
-        }, 2000);
-    </script>
-</body>
-</html>
